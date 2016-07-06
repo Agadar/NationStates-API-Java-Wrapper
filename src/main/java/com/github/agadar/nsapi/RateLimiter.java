6f08e958d8d197ffde784ef4f560ad272cbcb90b
martin_donkersloot@hotmail.com <martin_donkersloot@hotmail.com>
2016-07-06 15:37:07 +0200

    Implemented RateLimiter: now to use it in NationStatesAPI.

---
+++ src/main/java/com/github/agadar/nsapi/RateLimiter.java
@@ -0,0 +1,72 @@
+package com.github.agadar.nsapi;
+
+import com.sun.istack.internal.logging.Logger;
+
+/**
+ * Enforces rate limiting for x requests per y milliseconds.
+ * 
+ * @author Agadar <https://github.com/Agadar/>
+ */
+public class RateLimiter 
+{
+    /** The round buffer we're using, with length set to x in 'x requests per y milliseconds. */
+    private final long[] roundBuffer;
+    /** The y in 'x requests per y milliseconds'. */
+    private final long milliseconds;
+    /** The current index in the round buffer, starting at 0. */
+    private int index = 0;
+    
+    /**
+     * Constructs a new RateLimiter.
+     * 
+     * @param requests the x in 'x requests per y milliseconds'
+     * @param milliseconds the y in 'x requests per y milliseconds'
+     */
+    public RateLimiter(int requests, int milliseconds)
+    {
+        if (requests <= 0)
+        {
+            throw new IllegalArgumentException("'requests' should be > 0!");
+        }
+        
+        if (milliseconds <= 0)
+        {
+            throw new IllegalArgumentException("'milliseconds' should be > 0!");
+        }
+        
+        this.roundBuffer = new long[requests];
+        this.milliseconds = milliseconds;       
+    }
+    
+    /**
+     * Call this method before any code you want to enforce the rate limit on.
+     */
+    public synchronized void Await()
+    {
+        // Retrieve oldest and latest timestamps, calculate difference.
+        long oldest = roundBuffer[index];
+        long latest = System.currentTimeMillis();
+        long diff = latest - oldest;
+        
+        // If the difference is less than the y in 'x requests per y milliseconds'
+        // then print a warning and sleep for the duration of the difference.
+        if (diff < milliseconds)
+        {
+            long sleepFor = milliseconds - diff;
+            Logger.getLogger(RateLimiter.class).warning("Rate limit reached!"
+                        + " Thread put to sleep for " + sleepFor + " milliseconds.");           
+            try
+            {
+                Thread.sleep(sleepFor);
+            }
+            catch (InterruptedException ex)
+            {
+                throw new NationStatesAPIException("RateLimiter.class blew up!", ex);
+            }
+        }
+        
+        // Finally, update the oldest timestamp and increment the index.
+        roundBuffer[index] = latest;
+        index = ++index % roundBuffer.length;
+    }
+}
--- src/main/java/com/github/agadar/nsapi/TestMain.java
+++ src/main/java/com/github/agadar/nsapi/TestMain.java
@@ -1,8 +1,5 @@
 package com.github.agadar.nsapi;
 
-import com.github.agadar.nsapi.domain.world.World;
-import com.github.agadar.nsapi.enums.shard.WorldShard;
-
 /**
  *
  * @author Agadar <https://github.com/Agadar/>
@@ -16,9 +13,14 @@ public class TestMain
     public static void main(String[] args) throws NationStatesAPIException
     {
         NationStatesAPI ns = new NationStatesAPI();
-        World w = ns.world(WorldShard.CensusDescriptions);
-        System.out.println(w.Regions() == null);
-        System.out.println(w.RegionsByTag() == null);
+        RateLimiter rl = new RateLimiter(50, 30000);
+        int counter = 0;
+        
+        while (true)
+        {
+            rl.Await();
+            System.out.println("Called " + ++counter + " times!");
+        }
     }
     
 }
