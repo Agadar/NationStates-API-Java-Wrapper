c28dc3afbdc6871c0f8175cb5b8cf8c6df907504
Agadar <martin_donkersloot@hotmail.com>
2016-09-22 17:27:47 +0200

    Added 'from' option for TGCANRECRUIT and TGCANCAMPAIGN nation shards.

--- src/main/java/com/github/agadar/nsapi/query/NationQuery.java
+++ src/main/java/com/github/agadar/nsapi/query/NationQuery.java
@@ -12,6 +12,9 @@ import com.github.agadar.nsapi.enums.shard.NationShard;
  */
 public final class NationQuery extends CensusQuery<NationQuery, Nation, NationShard>
 {
+    /** See canReceiveTelegramFromRegion(...). */
+    private String canReceiveTelegramFromRegion;
+    
     /**
      * Constructor. Sets the name of the nation to query.
      * 
@@ -20,6 +23,20 @@ public final class NationQuery extends CensusQuery<NationQuery, Nation, NationSh
     public NationQuery(String nationName)
     {
         super(nationName);
+    }
+    
+    /**
+     * For use with the 'CanReceiveRecruitmentTelegrams' and
+     * 'CanReceiveCampaignTelegrams' shards. Allows a sender region to be
+     * specified.
+     * 
+     * @param region
+     * @return 
+     */
+    public final NationQuery canReceiveTelegramFromRegion(String region)   
+    {
+        this.canReceiveTelegramFromRegion = region;
+        return this;
     }
     
     @Override
@@ -37,5 +54,17 @@ public final class NationQuery extends CensusQuery<NationQuery, Nation, NationSh
         {
             throw new NationStatesAPIException("No or empty nation name supplied!");
         }
+    }
+
+    @Override
+    protected String buildURL()
+    {
+        String url = super.buildURL();
+        
+        // If canReceiveTelegramFromRegion was supplied, append them to url
+        if (canReceiveTelegramFromRegion != null && !canReceiveTelegramFromRegion.isEmpty())
+            url += "&from=" + canReceiveTelegramFromRegion;
+        
+        return url;
     }
 }
